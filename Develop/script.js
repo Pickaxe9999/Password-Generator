0bacc69604dd78ee08f50d4da6d44cb05d65d2bf
Jacob H <jacob.hannah164@gmail.com>
2022-05-18 09:27:15 -0400

    prompt the user for length, types, and exceptions

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,29 @@
 // Assignment code here
+var generatePassword = function(){
 
+  //prompt user for the password length
+  var passwordLength = window.prompt("Please enter the desired length for your password between 8 and 128 Characters.");
+  if(Number(passwordLength) === NaN){
+    window.alert("Please enter a valid number!");
+    writePassword();
+  }else if(Number(passwordLength)%1 === 0)){
+    window.alert("Please enter a whole number!");
+    writePassword();
+  }
+
+  var ifCapital = window.confirm("Would you like to include capital letters? OK = yes | cancel = no");
+  var ifNum = window.confirm("Would you also like to include numbers? OK = yes | cancel = no");
+  var ifSpecial = window.confirm("Would you like to include special characters? EX- &,*,%,#, etc. OK = yes | cancel = no");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
